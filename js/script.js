c3c9260dc7d9e8a463509164303aa2645b15c2f4
Etudiant <Etudiant>
2016-11-10 10:37:59 +0100

    Exercice n5 ver 1.0

---
+++ js/script.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){
+
+
+	// action sur le bouton d'envoi
+		$('button').on('click', function(event){
+
+			// suppression du comportement par defaut
+				event.preventDefault();
+
+			// recuperation du formulaire et serialisation de celui-ci
+
+				var formulaire = $('form');
+				var data = formulaire.serialize();
+
+			// envoie du formulaire au fichier de traitement PHP
+				console.log(formulaire);
+
+				console.log(data);
+
+				$.ajax({
+					type: 'POST',
+					url: 'traitement.php',
+					data: {'donnees':data},
+					// dataType: 'html',
+					dataType: 'json',
+
+					success : function(reponse, statut){
+
+						// affichage du contenu du data
+							console.log(reponse);
+							$('#prenom').val(reponse['prenom']);
+							$('textarea').val(reponse['message']);
+							console.log('Etat de la requete : '+statut);
+
+					// fin du success
+					},
+
+
+					error : function (resultat, statut, erreur){
+						console.log("reponse jquery : " + resultat);
+						console.log("statut de la requete : "+statut);
+						console.log("type d'erreur : "+erreur);
+						$('.erreur').html('Errreur : ('+erreur+')')
+
+					// fin de la gestion d'erreur
+					},
+
+				// fin de la fonction ajax
+				})
+
+
+
+		// fin de la fonction clic sur le BOUTON
+		});
+
+	// activation du bouton du formulaire
+		$('.form').on('keyup', function(){
+			$('#button').css('display','initial');
+		})
+
+
+
+	/*
+			touche()
+		creation de la fonction de controle du nbr de caractère
+	*/ 
+		function touche() {
+
+				// calcule du nombre de caractère
+					var nbrCaractere = $(this).val().length;
+
+				// calcule le nombre de cararactère restant
+					let restant = 140-nbrCaractere;
+
+				// affichage du nombre de caractère
+					var msg = ' '+restant+' caractères sur 140';
+
+					$('#caraRest').text(msg);
+
+					if restant >0 {
+						$('#button').css('display','initial');
+
+					} else { $('#button').css('display','none');}
+		// fin de la fonction de comptage des caractères
+		};
+
+	// comptage des caractères
+		$('#message').on('keyup', touche);
+		$('#message').on('keydown', touche);
+
+
+
+
+
+
+
+
+
+
+
+
+// fin du script
+})
+
+
+
---
+++ traitement.php
@@ -0,0 +1,43 @@
+<?php 
+/*
+	fichier du traitement du formulaire
+*/
+
+	// verification d'un POST
+	if ( ! empty($_POST) ) {
+
+
+		// post non vide on le recupere
+			$chaine = $_POST['donnees'];
+
+
+		// on retranscrit la chaine récupere en tableau pour recupere les donnée
+			parse_str($chaine, $tableau);
+
+			$prenom = $tableau['prenom'];
+			$message = $tableau['message'];
+		// on transphorme les données pour inverser l'ordre des lettres
+			$prenom_inv = strrev($prenom);
+			$message_inv = strrev($message);
+		// on met le resultat sous forme de tableau
+			$nouv_tableau = array(
+					'prenom' => $prenom_inv,
+					'message' => $message_inv
+				);
+
+		// on transphorme le tableau en json
+			$tableau_json = json_encode($nouv_tableau);
+
+		// on prepare l'envoi
+			$reponse = $tableau_json;
+
+
+		
+		// renvoi de la reponse sous forme de tableau
+			echo $reponse;
+			exit;
+
+
+	} else { echo 'erreur0'; exit; }
+
+	echo 'erreur1'; exit;
